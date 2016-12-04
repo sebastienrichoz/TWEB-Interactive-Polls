0874c28fc463de2b6cf43922265317f8ec145ef2
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2016-12-04 18:33:38 +0100

    directive to display number with thousands separator

---
+++ src/app/pipes/thousand-separator.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ThousandSeparatorPipe } from './thousand-separator.pipe';
+
+describe('Pipe: ThousandSeparator', () => {
+  it('create an instance', () => {
+    let pipe = new ThousandSeparatorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
